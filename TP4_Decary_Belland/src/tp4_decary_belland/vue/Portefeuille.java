a8e2ae4358096707024949d1fb955d1a6dbac134
charlotte decary <charlottedecary@MacBook-de-charlotte-2.local>
2018-05-25 11:09:01 +0200

    creation hashmpa

--- TP4_Decary_Belland/src/tp4_decary_belland/vue/Portefeuille.java
+++ TP4_Decary_Belland/src/tp4_decary_belland/vue/Portefeuille.java
@@ -3,12 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+//import java.util.Enumeration;
+import java.util.*;
+
 package tp4_decary_belland.vue;
 
 /**
  *
  * @author eugeniebelland
  */
+
+
 public class Portefeuille {
+    
+    Hashtable fond = new hashtable;
+    Hashtable instrument = new hashtable;
     
 }
